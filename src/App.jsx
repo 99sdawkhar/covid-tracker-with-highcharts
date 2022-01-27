bc0a0de802c0212ac36e6108f51a77b6eacdecac
dshubhamprdxn <shubhamd@axioned.com>
2022-01-27 23:44:43 +0530

    feat: initial structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import { ThemeProvider } from "styled-components";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
-const App = () => {
+import theme from "./themes/index";
+import Homepage from "./pages/Homepage";
+
+function App() {
+
   return (
-    <div>
-      Covid-19 Tracker
-    </div>
-  )
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route
+            path="*"
+            element={<Navigate replace to="/"/>}
+          />
+        </Routes>
+      </Router>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-import App from './App.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import GlobalStyle from './themes/global-styles';
 
-ReactDom.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <GlobalStyle />
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
